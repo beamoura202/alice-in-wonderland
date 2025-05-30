57b8245f7a5ad557bb3e2080006b27424b0bf11f
beamoura202 <beatrizmoura.slb@gmail.com>
2025-05-30 15:44:34 +0100

    cap6
    cap6cena2

--- js/script6.js
+++ js/script6.js
@@ -4,11 +4,13 @@ document.addEventListener('DOMContentLoaded', function () {
     const arvoreEsq = document.getElementById('arvoreesq');
     const planta = document.getElementById('planta');
     const gato = document.getElementById('gato');
+    const gatoCab = document.getElementById('gato-cabeca');
+    const gatoCorp = document.getElementById('gato-corpo');
     const alice = document.getElementById('alice');
 
     const fundoImg = document.getElementById('fundo-img');
     const fundoArvore = document.getElementById('fundo-arvore');
-        const vegetacaoContainer = document.getElementById('vegetacao-container'); // << ADICIONE ESTA LINHA
+    const vegetacaoContainer = document.getElementById('vegetacao-container'); // << ADICIONE ESTA LINHA
 
 
     window.addEventListener('scroll', function () {
@@ -35,7 +37,7 @@ document.addEventListener('DOMContentLoaded', function () {
         const scrollProgress = Math.abs(containerRect.top) / (containerRect.height - window.innerHeight);
 
         if (scrollProgress >= 0.3 && scrollProgress <= 0.4) {
-            
+
             arvoreDir.classList.add('move-left2a');
             planta.classList.add('move-left2p');
             gato.classList.add('move-right2g');
@@ -43,7 +45,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
             fundoArvore.classList.add('move-left3');
             fundoImg.classList.add('move-left4');
-              arvoreDir.classList.add('move-ad');
+            arvoreDir.classList.add('move-ad');
             arvoreEsq.classList.add('move-ae');
             planta.classList.add('move-p');
             gato.classList.add('move-g');
@@ -54,41 +56,78 @@ document.addEventListener('DOMContentLoaded', function () {
             arvoreDir.classList.remove('move-left2a');
             planta.classList.remove('move-left2p');
             gato.classList.remove('move-right2g');
-            alice.classList.remove('move-leftalice');
+            alice.classList.remove('move-left2alice');
 
             fundoArvore.classList.remove('move-left3');
             fundoImg.classList.remove('move-left4');
 
 
         }
-        });
-     window.addEventListener('scroll', function () {
+    });
+    window.addEventListener('scroll', function () {
         const containerRect = vegetacaoContainer.getBoundingClientRect();
         const scrollProgress = Math.abs(containerRect.top) / (containerRect.height - window.innerHeight);
 
-    if (scrollProgress >= 0.4 && scrollProgress <= 0.5) {
-        arvoreDir.classList.add('move-right2');
-        arvoreEsq.classList.add('move-right2e');
-        planta.classList.add('move-right2p');
-        gato.classList.add('move-right2g');
-        alice.classList.add('move-right2al');
+        if (scrollProgress >= 0.4 && scrollProgress <= 0.5) {
+            arvoreDir.classList.add('move-right2');
+            arvoreEsq.classList.add('move-right2e');
+            planta.classList.add('move-right2p');
+            gato.classList.add('move-right2g');
+            alice.classList.add('move-right2al');
 
-        fundoArvore.classList.add('move-right3');
-        fundoImg.classList.add('move-right4');
+            fundoArvore.classList.add('move-right3');
+            fundoImg.classList.add('move-right4');
 
-    } else {
+        } else {
 
 
-        arvoreDir.classList.remove('move-right2');
-        arvoreEsq.classList.remove('move-right2e');
-        planta.classList.remove('move-right2p');
-        gato.classList.remove('move-right2g');
-        alice.classList.remove('move-right2al');
+            arvoreDir.classList.remove('move-right2');
+            arvoreEsq.classList.remove('move-right2e');
+            planta.classList.remove('move-right2p');
+            gato.classList.remove('move-right2g');
+            alice.classList.remove('move-right2al');
 
-        fundoArvore.classList.remove('move-right3');
-        fundoImg.classList.remove('move-right4');
-    }
-});
+            fundoArvore.classList.remove('move-right3');
+            fundoImg.classList.remove('move-right4');
+        }
+    });
+
+
+
+    window.addEventListener('scroll', function () {
+        const containerRect = vegetacaoContainer.getBoundingClientRect();
+        const scrollProgress = Math.abs(containerRect.top) / (containerRect.height - window.innerHeight);
+
+        if (scrollProgress >= 0.6) {
+
+            // Cabeça aparece suavemente
+            // Gato desaparece suavemente
+            gato.classList.add('fade-out');
+            gatoCab.classList.add('show');
+
+
+
+
+        } else {
+            gatoCab.classList.remove('show');
+
+            gato.classList.remove('fade-out');
+            gato.classList.add('show');
+
+
+            gatoCab.classList.add('hide');
+
+        }
+
+
+
+        if (scrollProgress >= 0.65) {
+            gatoCab.classList.add('oscilando');
+        }
+        else {
+            gatoCab.classList.remove('oscilando');
+        }
+    });
 });
 
 
